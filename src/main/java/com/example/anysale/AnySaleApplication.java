86e38cdd248d16a39ce2e105d57aa7750a0b47ca
PG Kim <vakon1@daum.net>
2024-10-15 09:37:56 +0900

    ...

---
+++ src/main/java/com/example/anysale/AnySaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.anysale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnySaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnySaleApplication.class, args);
+    }
+
+}
